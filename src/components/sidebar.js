dc954ec08660235b743082e3f4dc4e6d938a4dfd
Austin Knobloch <austin.knobloch@samaritanministries.org>
2018-08-20 14:58:33 -0500

    remove accordian

--- src/components/sidebar.js
+++ src/components/sidebar.js
@@ -1,15 +1,20 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom'
 
-import { Accordion, AccordionItem } from 'react-sanfona'
 import logo from "../img/dash-donkey.svg";
 
 class Sidebar extends React.Component {
 
   constructor(props) {
     super(props);
-    this.state = {showSidebar: false};
+    this.state = {showSidebar: false,
+                  showDashing: true,
+                  showComponents: true,
+                  showStyle: true};
     this.handleClick = this.handleClick.bind(this);
+    this.toggleDashing = this.toggleDashing.bind(this);
+    this.toggleComponents = this.toggleComponents.bind(this);
+    this.toggleStyle = this.toggleStyle.bind(this);
   }
 
    handleClick() {
@@ -18,15 +23,25 @@ class Sidebar extends React.Component {
     }));
   }
 
-  render() {
+  toggleDashing() {
+    this.setState(prevState => ({
+      showDashing: !prevState.showDashing
+    }));
+  }
 
-    const isActiveTest = (match, location) => {
-      // if (!match) {
-      //   return false
-      // }
-      console.log()
-      //return location.pathname.includes(this.props.children)
-    }
+  toggleComponents() {
+    this.setState(prevState => ({
+      showComponents: !prevState.showComponents
+    }));
+  }
+
+  toggleStyle() {
+    this.setState(prevState => ({
+      showStyle: !prevState.showStyle
+    }));
+  }
+
+  render() {
 
     return (
 
@@ -39,48 +54,51 @@ class Sidebar extends React.Component {
         </div>
 
         <div className={this.state.showSidebar ? 'example-sidebar show' : 'example-sidebar'}>
-          <Accordion allowMultiple={true}>
+
+          <ul>
             <div style={{ textAlign: "center" }}>
               <img src={logo} alt="Dash the Donkey" style={{ width: "8rem" }}/>
             </div>
 
-            <AccordionItem title="Dashing" expanded={true}>
-            <i className="dashing-icon"></i>
+            <div style={{ cursor: "pointer" }} onClick={this.toggleDashing}>
+              <h4 className="group-title">Dashing</h4>
+              <i className={this.state.showDashing ? 'dashing-icon dashing-icon--arrow-down' : 'dashing-icon dashing-icon--arrow-right'}></i>
 
-              <div className="menu-group">
+              <div className={this.state.showDashing ? 'menu-group expanded' : 'menu-group'}>
                 <NavLink exact to="/" activeClassName="active">Principles</NavLink>
                 <NavLink exact to="/get-started" activeClassName="active">Get Started</NavLink>
                 <NavLink exact to="/resources" activeClassName="active">Resources</NavLink>
               </div>
-
-            </AccordionItem>
-
-            <AccordionItem title="Style" expanded={true}>
-            <i className="dashing-icon"></i>
-
-            <div className="menu-group">
-              <NavLink strict to="/style/color" activeClassName="active">Color</NavLink>
-              <NavLink strict to="/style/typography" activeClassName="active">Typography</NavLink>
             </div>
 
-            </AccordionItem>
+            <div style={{ cursor: "pointer" }} onClick={this.toggleComponents}>
+              <h4 className="group-title">Style</h4>
+              <i className={this.state.showComponents ? 'dashing-icon dashing-icon--arrow-down' : 'dashing-icon dashing-icon--arrow-right'}></i>
 
-            <AccordionItem title="Components" expanded={true}>
-            <i className="dashing-icon"></i>
-
-            <div className="menu-group">
-              <NavLink strict to="/components/actions/code" isActive={isActiveTest} activeClassName="active" title="actions">Actions</NavLink>
-              <NavLink strict to="/components/banner/code" isActive={isActiveTest} activeClassName="active">Banner</NavLink>
-              <NavLink strict to="/components/cards/code" isActive={isActiveTest} activeClassName="active">Cards</NavLink>
-              <NavLink strict to="/components/checkboxes/code" isActive={isActiveTest} activeClassName="active">Checkboxes</NavLink>
-              <NavLink strict to="/components/clippy/code" isActive={isActiveTest} activeClassName="active">Clippy</NavLink>
-              <NavLink strict to="/components/footer/code" isActive={isActiveTest} activeClassName="active">Footer</NavLink>
-              <NavLink strict to="/components/forms/code" isActive={isActiveTest} activeClassName="active">Forms</NavLink>
+              <div className={this.state.showComponents ? 'menu-group expanded' : 'menu-group'}>
+                <NavLink strict to="/style/color" activeClassName="active">Color</NavLink>
+                <NavLink strict to="/style/typography" activeClassName="active">Typography</NavLink>
+              </div>
             </div>
 
-            </AccordionItem>
+            <div style={{ cursor: "pointer" }} onClick={this.toggleStyle}>
 
-          </Accordion>
+              <h4 className="group-title">Components</h4>
+              <i className={this.state.showStyle ? 'dashing-icon dashing-icon--arrow-down' : 'dashing-icon dashing-icon--arrow-right'}></i>
+
+              <div className={this.state.showStyle ? 'menu-group expanded' : 'menu-group'}>
+                <NavLink strict to="/components/actions/code" activeClassName="active" title="actions">Actions</NavLink>
+                <NavLink strict to="/components/banner/code" activeClassName="active">Banner</NavLink>
+                <NavLink strict to="/components/cards/code" activeClassName="active">Cards</NavLink>
+                <NavLink strict to="/components/checkboxes/code" activeClassName="active">Checkboxes</NavLink>
+                <NavLink strict to="/components/clippy/code" activeClassName="active">Clippy</NavLink>
+                <NavLink strict to="/components/footer/code" activeClassName="active">Footer</NavLink>
+                <NavLink strict to="/components/forms/code" activeClassName="active">Forms</NavLink>
+              </div>
+
+            </div>
+
+          </ul>
         </div>
       </div>
     )
