0bdbf41810c78cea6e7b89c159c4f4f6f03401db
Austin Knobloch <austin.knobloch@samaritanministries.org>
2018-09-04 16:21:42 -0500

    add guidelines to checkbox, cards, and banners

--- src/pages/components/banner/guidelines.js
+++ src/pages/components/banner/guidelines.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'gatsby'
 
 import Layout from '../../../components/layout'
 import SubNav from '../../../components/subnavigation'
@@ -14,7 +15,54 @@ export default () => (
       <SubNav pageName={currentPageNameLower}/>
     </header>
     <AppContent>
-      <p>Some Banner Guidelines</p>
+      <div className="row">
+        <div className="column column--full">
+          <p className="intro">Banners can be used to show important information to a user. Card banners should have text that is directly related to an action that has been completed or is about to be performed.</p>
+
+          <h2 className="has-number has-number--one no-margin--top">Banner Types</h2>
+            <p>Common types of card banners are success, warning, error, and generic. Like <Link to="/components/actions/guidelines">Actions</Link>, banners should use purposeful color when using them.</p>
+            <div className="image-container double-padding">
+              <div className="banner-container">
+                <div className="card-banner">
+                  <p>This is a card banner</p>
+                </div>
+                <div className="card-banner has-success">
+                  <i className="dashing-icon dashing-icon--checkmark-filled"></i>
+                  <p>Your file is ready to upload</p>
+                </div>
+                <div className="card-banner has-warning">
+                  <i className="dashing-icon dashing-icon--info-filled"></i>
+                  <p>You may only submit bill information and upload images for one statement at a time. If you have multiple statements to submit, please enter them separately.</p>
+                </div>
+                <div className="card-banner has-error">
+                  <i className="dashing-icon dashing-icon--alert-filled"></i>
+                  <p>You must complete all fields below before continuing</p>
+                </div>
+              </div>
+            </div>
+
+            <blockquote className="tips" style={{ marginTop: "2rem" }}>
+              <strong>Tip: Icons in Banners</strong>
+              <p>To add more attention to card banners, use <Link to="components/icons/code">icons</Link> before the banner text. Banners are setup to automatically add the appropriate spacing.</p>
+            </blockquote>
+
+            <h2 className="has-number has-number--two">Placement</h2>
+            <p>When using a card banner, it should be placed directly under the card header to draw the user's attention right away.</p>
+            <div className="image-container double-padding" style={{ marginBottom: "2rem" }}>
+              <div className="card card--example-titles">
+                <div className="card-header">
+                  <h3>Membership Options</h3>
+                </div>
+                <div className="card-content">
+                  <div className="card-banner">
+                    <p>This is a card banner</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+        </div>
+      </div>
     </AppContent>
   </Layout>
 )
--- src/pages/components/cards/guidelines.js
+++ src/pages/components/cards/guidelines.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'gatsby'
 
 import Layout from '../../../components/layout'
 import SubNav from '../../../components/subnavigation'
@@ -14,7 +15,58 @@ export default () => (
       <SubNav pageName={currentPageNameLower}/>
     </header>
     <AppContent>
-      <p>Some Banner Guidelines</p>
+      <div className="row">
+        <div className="column column--full">
+          <p className="intro">Cards are used to visually group certain topics of information. This makes it easier for users to quickly find and compare information of interest. Avoid using to much content within a card to overload the user.</p>
+
+          <h2 className="has-number has-number--one no-margin--top">Buttons within Cards</h2>
+            <p>Call to actions within cards should be placed in the card footer and left aligned. If there is only one action in the footer, this should be center aligned. This pattern supports the <Link to="https://www.nngroup.com/articles/f-shaped-pattern-reading-web-content/">F-pattern layout</Link>.</p>
+            <div className="image-container double-padding">
+              <div className="card card--example-footer">
+                <div className="card--footer" style={{ paddingTop: "1rem" }}>
+                  <button className="button button--green button--smooth">Submit</button>
+                  <button className="button button--secondary button--transparent">Cancel</button>
+                </div>
+              </div>
+            </div>
+
+          <blockquote className="tips" style={{ marginTop: "2rem" }}>
+            <strong>Tip: Number of Actions</strong>
+            <p>Be careful to not add to many actions to the card footer. It is recommended to have one primary action and no more than two secondary actions. Follow the <Link to="/product/components/actions/guidelines">button hierarchy</Link> when adding more then one action.</p>
+          </blockquote>
+
+          <h2 className="has-number has-number--two">Card Titles</h2>
+            <p>These give the user a better idea of what the content of the card is. Card titles should be short and to the point. Use one to three words that quickly describe what the user will see.</p>
+              <div className="image-container double-padding" style={{ marginBottom: "2rem" }}>
+                <div className="flex-container">
+                  <div className="icon-container">
+                    <i className="dashing-icon dashing-icon--checkmark-filled dashing-icon--green"></i>
+                  </div>
+                  <div className="card card--example-titles">
+                    <div className="card--header has-border">
+                      <h3>Membership Options</h3>
+                    </div>
+                    <div className="card--content">
+                    </div>
+                  </div>
+                </div>
+                <br/>
+                <div className="flex-container">
+                  <div className="icon-container">
+                    <i className="dashing-icon dashing-icon--alert-filled dashing-icon--red"></i>
+                  </div>
+                  <div className="card card--example-titles">
+                    <div className="card--header has-border">
+                      <h3>Information about your Membership</h3>
+                    </div>
+                    <div className="card--content">
+                    </div>
+                  </div>
+                </div>
+            </div>
+
+        </div>
+      </div>
     </AppContent>
   </Layout>
 )
