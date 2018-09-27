6d1d2df040da14c9df6f17165ec5d0b0933274c2
Austin Knobloch <austin.knobloch@samaritanministries.org>
2018-09-27 09:47:04 -0500

    Add footer code example (#14)

--- src/pages/components/footer/code.js
+++ src/pages/components/footer/code.js
@@ -3,6 +3,8 @@ import React from 'react'
 import Layout from '../../../components/layout'
 import SubNav from '../../../components/subnavigation'
 import AppContent from '../../../components/appcontent'
+import CodeToggle from '../../../components/codetoggle'
+import CodeToggleSCSS from '../../../components/codetoggle-scss'
 
 const currentPageName = "Footer";
 const currentPageNameLower = currentPageName.toLowerCase();
@@ -16,6 +18,31 @@ export default () => (
     <AppContent>
       <div className="row">
         <div className="column column--full">
+          <h2 className="example-header no-margin--top">Footer <button className="button button--transparent button--copy-link" data-id="copyurl" id="Footer"/></h2>
+          <div className="row example-container">
+            <div className="column column--full" style={{ overflow: "auto" }}>
+              <footer className="app-footer example--footer" style={{ position: "relative" }}>
+                <div className="row no-padding">
+                  <button className="button button--primary float-left">Save</button>
+                  <button className="button button--primary button--green float-right">Submit</button>
+                </div>
+              </footer>
+            </div>
+          </div>
+          <CodeToggle>
+{`<!-- Apply the fixed class to the app-footer element to fix it to the bottom -->
+<footer class="app-footer fixed">
+  <div class="row no-padding">
+    <button class="float-left">Save</button>
+    <button class="button--green float-right">Submit</button>
+  </div>
+</footer>`}
+          </CodeToggle>
+          <CodeToggleSCSS>
+{`//Include these variables in your theme file to change the color theme of your footer
+$footer-color: $white !default; //Override this variable to change the color of your footer
+$footer-border-color: $gray-200 !default; //Override this variable to change the color of the top border of your footer`}
+          </CodeToggleSCSS>
 
         </div>
       </div>
