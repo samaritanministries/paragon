944b300df26d583a546f3a667df2deac24bbfbcd
Austin Knobloch <austin.knobloch@samaritanministries.org>
2018-09-27 13:37:52 -0500

    Add footer to all pages

--- src/components/layout.js
+++ src/components/layout.js
@@ -8,6 +8,12 @@ export default ({ children }) => (
     <Sidebar />
     <div className="app-content">
       {children}
+      <footer>
+        <div className="copyright">
+          © 2018 Samaritan Ministries International
+        </div>
+        <a href="https://github.com/samaritanministries/dashing" target="_blank" rel="noopener noreferrer">Download on GitHub</a>
+      </footer>
     </div>
   </div>
 )
