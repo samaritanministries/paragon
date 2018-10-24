12d39018f15f1c6c64b6e3370a14fe2096409a2d
Austin Knobloch <austin.knobloch@samaritanministries.org>
2018-10-24 14:26:20 -0500

    Initial accordion addition

---
+++ src/pages/components/accordion/code.js
@@ -0,0 +1,58 @@
+import React from 'react'
+import $ from 'jquery';
+
+import Layout from '../../../components/layout'
+import SubNav from '../../../components/subnavigation'
+import AppContent from '../../../components/appcontent'
+import CodeToggle from '../../../components/codetoggle'
+
+const currentPageName = "Accordion";
+const currentPageNameLower = currentPageName.toLowerCase();
+
+class AccordionCode extends React.Component {
+
+  componentDidMount() {
+    $("#toggleAccordion").click(function(){
+      this.parentElement.classList.toggle('expanded');
+      $(".accordion--content").slideToggle(200);
+    });
+  }
+
+  render() {
+
+    return (
+
+      <Layout>
+        <header className="subnav">
+          <h1>{currentPageName}</h1>
+          <SubNav pageName={currentPageNameLower}/>
+        </header>
+        <AppContent>
+          <div className="row">
+            <div className="column column--full">
+              <h2 className="example-header no-margin--top">Accordion <button className="button button--transparent button--copy-link" data-id="copyurl" id="Default_Spinner"/></h2>
+              <div className="row example-container">
+                <div className="column column--full">
+                  <div class="accordion" tabindex="0">
+                    <div className="accordion--header" id="toggleAccordion">
+                      <i className="dashing-icon accordion--arrow"></i>
+                      <h3 className="title">Accordion Title</h3>
+                    </div>
+
+                    <div className="accordion--content">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</div>
+
+                  </div>
+                </div>
+              </div>
+              <CodeToggle>
+{`<div class="dash-spinner"></div>`}
+              </CodeToggle>
+
+            </div>
+          </div>
+        </AppContent>
+      </Layout>
+    )
+  }
+}
+export default AccordionCode;
