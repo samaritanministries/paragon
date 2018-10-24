ac8ce960318114f353c532d776285bc547ab7898
Austin Knobloch <austin.knobloch@samaritanministries.org>
2018-10-24 14:26:20 -0500

    Initial accordion addition

---
+++ src/pages/components/accordion/guidelines.js
@@ -0,0 +1,25 @@
+import React from 'react'
+
+import Layout from '../../../components/layout'
+import SubNav from '../../../components/subnavigation'
+import AppContent from '../../../components/appcontent'
+import StillWorking from '../../../components/stillworking'
+
+const currentPageName = "Spinner";
+const currentPageNameLower = currentPageName.toLowerCase();
+
+export default () => (
+  <Layout>
+    <header className="subnav"> 
+      <h1>{currentPageName}</h1>
+      <SubNav pageName={currentPageNameLower}/>
+    </header>
+    <AppContent>
+      <div className="row">
+        <div className="column column--full">
+          <StillWorking />
+        </div>
+      </div>
+    </AppContent>
+  </Layout>
+)
