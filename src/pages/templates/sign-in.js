b514f7fcae42375870e13c8e4462be47b7edf7c3
Jared Rauh <jared.rauh@samaritanministries.org>
2018-11-28 12:00:54 -0600

    added sign-in template

---
+++ src/pages/templates/sign-in.js
@@ -0,0 +1,33 @@
+import React from 'react'
+
+export default () => (
+
+	<div className="grid-phoenix fullscreen">
+
+    <section className="app ex-no-color">
+			<div className="content content--reader-width center-align">
+	      <h1>Sign In</h1>
+	      <p>or <a>create an account</a></p>
+				<fieldset>
+					<label>Email</label>
+					<input type="email" for="email"/>
+				</fieldset>
+				<fieldset>
+					<label>Password</label>
+					<input type="password" for="password"/>
+				</fieldset>
+			  <span className="space-between">
+					<fieldset className="checkbox--custom">
+				    <input type="checkbox" id="remember-me"/>
+				    <label for="remember-me">Remember me</label>
+				  </fieldset>
+					<button className="button button--transparent">
+						Forgot your password
+					</button>
+				</span>
+				<button className="button button-block">Sign in</button>
+			</div>
+    </section>
+  </div>
+
+)
