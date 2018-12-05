c13a4832faa7babda27567511f52afae182692a8
Jared Rauh <jared.rauh@samaritanministries.org>
2018-12-05 08:25:19 -0600

    updated sign up template

--- src/pages/templates/sign-up.js
+++ src/pages/templates/sign-up.js
@@ -2,43 +2,22 @@ import React from 'react'
 
 export default () => (
 <div>
-
-	<section class="mobile-overlay is-collapsed">
-		<aside class="mobile-sidebar">
-			<div class="mobile-profile">
-				<a href="/templates/mobile" class="mobile-sidebar--button" id="open-button" ><i class="dashing-icon dashing-icon--close"></i></a>
-				<a href="/templates/single-page" class="mobile-profile--account">
-					<div class="profile-image"><i class="dashing-icon dashing-icon--face-good"></i></div>
-					<div class="profile-edit"><i class="dashing-icon dashing-icon--pencil"></i></div>
-					<h3 class="profile-name">Nitsua Cholbonk</h3>
-					<span class="profile-email">nitsua.cholbonk@mailinator.com</span>
-				</a>
-			</div>
-			<ul class="mobile-sidebar-nav">
-				<li><i class="dashing-icon dashing-icon--locked"></i><a href="/">Dashboard</a></li>
-				<li><i class="dashing-icon dashing-icon--locked"></i><a href="/single-page">Single Column Layout in Dashboard</a></li>
-				<li><i class="dashing-icon dashing-icon--locked"></i><a href="/fullscreen">Fullscreen Layout in Dashboard</a></li>
-				<li><i class="dashing-icon dashing-icon--locked"></i><a href="/mobile">Open Mobile Menu</a></li>
-			</ul>
-		</aside>
-	</section>
-
-	<div class="grid-phoenix fullscreen">
-		<header class="header" style={{backgroundColor: "#FFFFFF"}}>
-			<div class="content content-m" style={{width: "100%"}}>
-				<div class="progress">
-					<div class="progress-bar progress-bar--2of3" data-id="progress-stepper"></div>
+	<div className="grid-phoenix fullscreen">
+		<header className="header signUpHeader">
+			<div className="content content-m" style={{width: "100%"}}>
+				<div className="progress">
+					<div className="progress-bar progress-bar--1of3" data-id="progress-stepper"></div>
 				</div>
-				<div class="progress-labels">
-					<div class="progress-label progress-label--completed"><a>Step 1</a></div>
-					<div class="progress-label"><a>Step 2</a></div>
-					<div class="progress-label"><a>Step 3</a></div>
+				<div className="progress-labels">
+					<div className="progress-label"><a>Membership</a></div>
+					<div className="progress-label"><a>Agreements</a></div>
+					<div className="progress-label"><a>Payment</a></div>
 				</div>
 			</div>
 		</header>
 
-		<section class="app">
-			<div class="content content-l">
+		<section className="app">
+			<div className="content content-l">
 				<div className="grid grid-padding">
 
 					<h1>Enter Information About Your Membership</h1>
@@ -46,7 +25,7 @@ export default () => (
 
 					<div className="grid--two-thirds">
 						<div className="grid card">
-							<span class="space-between">
+							<span className="space-between">
 								<h3>Household Information</h3>
 								<span>Step 1 of 5</span>
 							</span>
@@ -54,33 +33,33 @@ export default () => (
 							<hr />
 
 							<p>Please include every family member in your household, even if not in your membership.</p>
-							<h4>Your Information</h4>
+							<h4>Primary Information</h4>
 
-							<fieldset class="grid--third">
+							<fieldset className="grid--third">
 								<label for="first-name">First Name</label>
 								<input type="text" id="first-name"/>
 							</fieldset>
-							<fieldset class="grid--third">
+							<fieldset className="grid--third">
 								<label for="middle-name">Middle Name</label>
 								<input type="text" id="middle-name"/>
 							</fieldset>
-							<fieldset class="grid--third">
+							<fieldset className="grid--third">
 								<label for="last-name">Last Name</label>
 								<input type="text" id="last-name"/>
 							</fieldset>
 
-							<fieldset class="grid--third">
+							<fieldset className="grid--third">
 								<label for="date-of-birth">Date of Birth</label>
 								<input type="date" id="date-of-birth"/>
 							</fieldset>
-							<fieldset class="grid--third select--has-icon">
+							<fieldset className="grid--third select--has-icon">
 								<label for="gender">Gender</label>
 								<select id="gender">
 									<option>Male</option>
 									<option>Female</option>
 								</select>
 							</fieldset>
-							<fieldset class="grid--third select--has-icon">
+							<fieldset className="grid--third select--has-icon">
 								<label for="marital-status">Marital Status</label>
 								<select id="marital-status">
 									<option>Married</option>
@@ -88,7 +67,7 @@ export default () => (
 									<option>Divorced</option>
 								</select>
 							</fieldset>
-							<fieldset class="grid--two-thirds select--has-icon">
+							<fieldset className="grid--two-thirds select--has-icon">
 								<label for="occupation">Occupation</label>
 								<select id="occupation">
 									<option>Accounting</option>
@@ -96,14 +75,17 @@ export default () => (
 									<option>Teacher</option>
 								</select>
 							</fieldset>
-							<fieldset class="grid--third">
+							<fieldset className="grid--third">
 								<label for="ssn">Last 4 Digits of Social</label>
 								<input type="text" id="ssn"/>
 							</fieldset>
+							<div className="grid--third">
+								<button className="button--gray">+ Add Dependend</button>
+							</div>
 						</div>
 
 						<div className="grid card">
-							<span class="space-between">
+							<span className="space-between">
 								<h3>Membership Plan</h3>
 								<span>Step 2 of 5</span>
 							</span>
@@ -111,69 +93,69 @@ export default () => (
 							<hr />
 
 							<p>Please include every family member in your household, even if not in your membership.</p>
-							<ol class="ordered-list--custom">
+							<ol className="ordered-list--custom">
 							  <li className="no-margin"><h3>Membership Size</h3></li>
 								<p>How many people would you like in your membership</p>
 								<div className="memberSelector">
-								  <div class="checkbox-card">
+								  <div className="checkbox-card">
 								    <input type="checkbox" name="memberCheckbox" id="member1"/>
-								    <label for="member1" class="card">Ryan Fitz</label>
+								    <label for="member1" className="card">Ryan Fitz</label>
 								  </div>
-								  <div class="checkbox-card">
+								  <div className="checkbox-card">
 								    <input type="checkbox" name="memberCheckbox" id="member2" />
-								    <label for="member2" class="card">Jill Fitz</label>
+								    <label for="member2" className="card">Jill Fitz</label>
 								  </div>
-								  <div class="checkbox-card">
+								  <div className="checkbox-card">
 								    <input type="checkbox" name="memberCheckbox" id="member3" />
-								    <label for="member3" class="card">Jackson Fitz</label>
+								    <label for="member3" className="card">Jackson Fitz</label>
 								  </div>
-									<div class="checkbox-card">
+									<div className="checkbox-card">
 								    <input type="checkbox" name="memberCheckbox" id="member4" />
-								    <label for="member4" class="card">Emily Fitz</label>
+								    <label for="member4" className="card">Emily Fitz</label>
 								  </div>
-									<div class="checkbox-card">
+									<div className="checkbox-card">
 								    <input type="checkbox" name="memberCheckbox" id="member5" />
-								    <label for="member5" class="card">Karen Fitz</label>
+								    <label for="member5" className="card">Karen Fitz</label>
 								  </div>
 								</div>
 
 								<li className="no-margin mt-space-m"><h3>Co-Share</h3></li>
 								<p>Choose your Co-Share</p>
 								<div className="dialSelctor">
-								  <div class="radio-card">
+								  <div className="radio-card">
 								    <input type="radio" name="coShareRadio" id="coShare1"/>
-								    <label for="coShare1" class="card">10% + $2,500 Max</label>
+								    <label for="coShare1" className="card">10% + $2,500 Max</label>
 								  </div>
-								  <div class="radio-card">
+								  <div className="radio-card">
 								    <input type="radio" name="coShareRadio" id="coShare2" />
-								    <label for="coShare2" class="card">15% + $3,750 Max</label>
+								    <label for="coShare2" className="card">15% + $3,750 Max</label>
 								  </div>
-								  <div class="radio-card">
+								  <div className="radio-card">
 								    <input type="radio" name="coShareRadio" id="coShare3" />
-								    <label for="coShare3" class="card">20% + $5,000 Max</label>
+								    <label for="coShare3" className="card">20% + $5,000 Max</label>
 								  </div>
 								</div>
 
 								<li className="no-margin mt-space-m"><h3>Annual Unshareable Amount</h3></li>
 								<p>Select your annual personal responsibility</p>
 								<div className="dialSelctor">
-								  <div class="radio-card">
+								  <div className="radio-card">
 								    <input type="radio" name="AUARadio" id="AUA1"/>
-								    <label for="AUA1" class="card">$1,000</label>
+								    <label for="AUA1" className="card">$1,000</label>
 								  </div>
-								  <div class="radio-card">
+								  <div className="radio-card">
 								    <input type="radio" name="AUARadio" id="AUA2" />
-								    <label for="AUA2" class="card">$2,000</label>
+								    <label for="AUA2" className="card">$2,000</label>
 								  </div>
-								  <div class="radio-card">
+								  <div className="radio-card">
 								    <input type="radio" name="AUARadio" id="AUA3" />
-								    <label for="AUA3" class="card">$5,000</label>
+								    <label for="AUA3" className="card">$5,000</label>
 								  </div>
 								</div>
 
 							  <li className="no-margin mt-space-m"><h3>Start Date</h3></li>
 								<p>When would you like to start your membership?</p>
-								<fieldset className="grid--third select--has-icon">
+								<fieldset className="select--has-icon membershipStartDate">
 									<select type="text" id="first-name">
 										<option>Today</option>
 										<option>12/1/2018</option>
@@ -185,7 +167,7 @@ export default () => (
 						</div>
 
 						<div className="grid card">
-							<span class="space-between">
+							<span className="space-between">
 								<h3>Household Information</h3>
 								<span>Step 3 of 5</span>
 							</span>
@@ -195,31 +177,31 @@ export default () => (
 							<p>Please include every family member in your household, even if not in your membership.</p>
 							<h4>Your Information</h4>
 
-							<fieldset class="grid--third">
+							<fieldset className="grid--third">
 								<label for="first-name">First Name</label>
 								<input type="text" id="first-name"/>
 							</fieldset>
-							<fieldset class="grid--third">
+							<fieldset className="grid--third">
 								<label for="middle-name">Middle Name</label>
 								<input type="text" id="middle-name"/>
 							</fieldset>
-							<fieldset class="grid--third">
+							<fieldset className="grid--third">
 								<label for="last-name">Last Name</label>
 								<input type="text" id="last-name"/>
 							</fieldset>
 
-							<fieldset class="grid--third">
+							<fieldset className="grid--third">
 								<label for="date-of-birth">Date of Birth</label>
 								<input type="date" id="date-of-birth"/>
 							</fieldset>
-							<fieldset class="grid--third select--has-icon">
+							<fieldset className="grid--third select--has-icon">
 								<label for="gender">Gender</label>
 								<select id="gender">
 									<option>Male</option>
 									<option>Female</option>
 								</select>
 							</fieldset>
-							<fieldset class="grid--third select--has-icon">
+							<fieldset className="grid--third select--has-icon">
 								<label for="marital-status">Marital Status</label>
 								<select id="marital-status">
 									<option>Married</option>
@@ -227,7 +209,7 @@ export default () => (
 									<option>Divorced</option>
 								</select>
 							</fieldset>
-							<fieldset class="grid--two-thirds select--has-icon">
+							<fieldset className="grid--two-thirds select--has-icon">
 								<label for="occupation">Occupation</label>
 								<select id="occupation">
 									<option>Accounting</option>
@@ -235,14 +217,14 @@ export default () => (
 									<option>Teacher</option>
 								</select>
 							</fieldset>
-							<fieldset class="grid--third">
+							<fieldset className="grid--third">
 								<label for="ssn">Last 4 Digits of Social</label>
 								<input type="text" id="ssn"/>
 							</fieldset>
 						</div>
 
 						<div className="grid card">
-							<span class="space-between">
+							<span className="space-between">
 								<h3>Household Information</h3>
 								<span>Step 4 of 5</span>
 							</span>
@@ -252,31 +234,31 @@ export default () => (
 							<p>Please include every family member in your household, even if not in your membership.</p>
 							<h4>Your Information</h4>
 
-							<fieldset class="grid--third">
+							<fieldset className="grid--third">
 								<label for="first-name">First Name</label>
 								<input type="text" id="first-name"/>
 							</fieldset>
-							<fieldset class="grid--third">
+							<fieldset className="grid--third">
 								<label for="middle-name">Middle Name</label>
 								<input type="text" id="middle-name"/>
 							</fieldset>
-							<fieldset class="grid--third">
+							<fieldset className="grid--third">
 								<label for="last-name">Last Name</label>
 								<input type="text" id="last-name"/>
 							</fieldset>
 
-							<fieldset class="grid--third">
+							<fieldset className="grid--third">
 								<label for="date-of-birth">Date of Birth</label>
 								<input type="date" id="date-of-birth"/>
 							</fieldset>
-							<fieldset class="grid--third select--has-icon">
+							<fieldset className="grid--third select--has-icon">
 								<label for="gender">Gender</label>
 								<select id="gender">
 									<option>Male</option>
 									<option>Female</option>
 								</select>
 							</fieldset>
-							<fieldset class="grid--third select--has-icon">
+							<fieldset className="grid--third select--has-icon">
 								<label for="marital-status">Marital Status</label>
 								<select id="marital-status">
 									<option>Married</option>
@@ -284,7 +266,7 @@ export default () => (
 									<option>Divorced</option>
 								</select>
 							</fieldset>
-							<fieldset class="grid--two-thirds select--has-icon">
+							<fieldset className="grid--two-thirds select--has-icon">
 								<label for="occupation">Occupation</label>
 								<select id="occupation">
 									<option>Accounting</option>
@@ -292,14 +274,14 @@ export default () => (
 									<option>Teacher</option>
 								</select>
 							</fieldset>
-							<fieldset class="grid--third">
+							<fieldset className="grid--third">
 								<label for="ssn">Last 4 Digits of Social</label>
 								<input type="text" id="ssn"/>
 							</fieldset>
 						</div>
 
 						<div className="grid card">
-							<span class="space-between">
+							<span className="space-between">
 								<h3>Household Information</h3>
 								<span>Step 5 of 5</span>
 							</span>
@@ -309,31 +291,31 @@ export default () => (
 							<p>Please include every family member in your household, even if not in your membership.</p>
 							<h4>Your Information</h4>
 
-							<fieldset class="grid--third">
+							<fieldset className="grid--third">
 								<label for="first-name">First Name</label>
 								<input type="text" id="first-name"/>
 							</fieldset>
-							<fieldset class="grid--third">
+							<fieldset className="grid--third">
 								<label for="middle-name">Middle Name</label>
 								<input type="text" id="middle-name"/>
 							</fieldset>
-							<fieldset class="grid--third">
+							<fieldset className="grid--third">
 								<label for="last-name">Last Name</label>
 								<input type="text" id="last-name"/>
 							</fieldset>
 
-							<fieldset class="grid--third">
+							<fieldset className="grid--third">
 								<label for="date-of-birth">Date of Birth</label>
 								<input type="date" id="date-of-birth"/>
 							</fieldset>
-							<fieldset class="grid--third select--has-icon">
+							<fieldset className="grid--third select--has-icon">
 								<label for="gender">Gender</label>
 								<select id="gender">
 									<option>Male</option>
 									<option>Female</option>
 								</select>
 							</fieldset>
-							<fieldset class="grid--third select--has-icon">
+							<fieldset className="grid--third select--has-icon">
 								<label for="marital-status">Marital Status</label>
 								<select id="marital-status">
 									<option>Married</option>
@@ -341,7 +323,7 @@ export default () => (
 									<option>Divorced</option>
 								</select>
 							</fieldset>
-							<fieldset class="grid--two-thirds select--has-icon">
+							<fieldset className="grid--two-thirds select--has-icon">
 								<label for="occupation">Occupation</label>
 								<select id="occupation">
 									<option>Accounting</option>
@@ -349,25 +331,25 @@ export default () => (
 									<option>Teacher</option>
 								</select>
 							</fieldset>
-							<fieldset class="grid--third">
+							<fieldset className="grid--third">
 								<label for="ssn">Last 4 Digits of Social</label>
 								<input type="text" id="ssn"/>
 							</fieldset>
 						</div>
 
-						<div class="accordion">
-						  <div class="accordion--header" id="toggleAccordion" tabindex="0">
-						    <i class="dashing-icon accordion--arrow"></i>
-						    <h3 class="title">Accordion Title</h3>
+						<div className="accordion">
+						  <div className="accordion--header" id="toggleAccordion" tabindex="0">
+						    <i className="dashing-icon accordion--arrow"></i>
+						    <h3 className="title">Accordion Title</h3>
 						  </div>
-						  <div class="accordion--content">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</div>
+						  <div className="accordion--content">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</div>
 						</div>
 					</div>
 
 
 
 					<div className="grid--third">
-						<div className="card"><h3 class="center-align">Your Monthly Share</h3></div>
+						<div className="card"><h3 className="center-align">Your Monthly Share</h3></div>
 
 						<hr />
 
