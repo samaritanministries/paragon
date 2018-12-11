6791846bb34deb5aec27dc7d7c7dfc94665ee48c
Austin Knobloch <austin.knobloch@samaritanministries.org>
2018-12-11 11:51:03 -0600

    - Update Overpanel styling - Add transition to nav menu items

--- src/pages/templates/overpanel.js
+++ src/pages/templates/overpanel.js
@@ -3,12 +3,19 @@ import '../../sass/example-page/example-page.scss'
 
 export default () => (
 
-	<div>
+	<div className="overpanel">
 		<header className="title-bar">
+			<div className="title-content">
+				<div>
+					<h2>Overpanel Title</h2>
+					<p>Overpanel description</p>
+				</div>
+				<button className="button button--secondary">Close</button>
+			</div>
 		</header>
 
     <section className="app">
-			<div className="content content--reader-width">
+			<div className="content">
 				<div className="grid grid-padding">
 		      <h1>Example Fullscreen Page</h1>
 		      <p>This example shows a fullscreen layout. Simple! This example shows a fullscreen layout. Simple! This example shows a fullscreen layout. Simple! This example shows a fullscreen layout. Simple! This example shows a fullscreen layout. Simple! This example shows a fullscreen layout. Simple!</p>
