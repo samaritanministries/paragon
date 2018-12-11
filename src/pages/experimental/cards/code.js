6f207d3fafd8f91795b00587086272f2e6307c25
Jared Rauh <jared.rauh@samaritanministries.org>
2018-12-11 16:05:27 -0600

    updated experimental card example page

--- src/pages/experimental/cards/code.js
+++ src/pages/experimental/cards/code.js
@@ -14,16 +14,59 @@ export default () => (
       <h1>{currentPageName}</h1>
       <SubNav pageName={currentPageNameLower}/>
     </header>
-    <AppContent>
 
+    <AppContent>
     <main className="content content-m">
 			<div className="grid grid-padding">
+
         <div className="card no-padding">
-          <div className="card--header has-border">
-            <h2>Form</h2>
+          <div className="card-header has-border">
+            <h2>Card with List</h2>
+          </div>
+          <div className="card-content">
+            <ul className="text-list">
+              <li className="text-list_item">This is a simple card</li>
+              <li className="text-list_item">This is a simple card</li>
+            </ul>
+          </div>
+        </div>
+
+        <h2>Card States</h2>
+        <div className="example-container">
+          <div className="card is-selectable">
+            <div className="card-header">
+              <h3>Selectable</h3>
+            </div>
+            <div className="card-content">
+              <p>To use a selectable card, apply <code className="example-text">.is-selectable</code> to the <code className="example-text">.card</code>.</p>
+            </div>
           </div>
 
-          <div className="card--content flex-content">
+          <div className="card is-selectable is-disabled">
+            <div className="card-header">
+              <h3>Disabled</h3>
+            </div>
+            <div className="card-content">
+              <p>To use a disabled card, apply <code className="example-text">.is-disabled</code> to the <code className="example-text">.card</code>.</p>
+            </div>
+          </div>
+
+          <div className="card card--dashed">
+            <div className="card-header">
+              <h3>Dashed</h3>
+            </div>
+            <div className="card-content">
+              <p>To use a dashed card, apply <code className="example-text">.card--dashed</code> to the <code className="example-text">.card</code>.</p>
+            </div>
+          </div>
+        </div>
+
+        <div className="card">
+          <div className="card-header has-border">
+            <h2 className="no-margin">Form Card</h2>
+          </div>
+
+          <div className="card-content flex-content">
             <div className="flex-row">
               <fieldset>
                 <label for="first-name">First Name</label>
@@ -53,19 +96,19 @@ export default () => (
               <fieldset className="spacer"></fieldset>
             </div>
           </div>
-          <div className="card--footer">
+          <div className="card-footer">
             <button className="mr-space-xs">Submit</button>
             <button className="button--transparent button--gray">Cancel</button>
           </div>
         </div>
 
-        <div className="card no-padding">
-          <div className="card--header center-align">
+        <div className="card">
+          <div className="card-header center-align">
             <h2>Sign In</h2>
             <p>Sign in to your account</p>
           </div>
 
-          <div className="card--content flex-content">
+          <div className="card-content flex-content">
             <div className="flex-row">
               <fieldset>
                 <label for="email">Email</label>
@@ -90,39 +133,8 @@ export default () => (
               </button>
             </div>
           </div>
-          <div className="card--footer">
+          <div className="card-footer">
             <button className="button-block">Sign In</button>
-          </div>
-        </div>
-
-        <div className="card grid">
-          <h2>Grid Card</h2>
-          <hr />
-          <fieldset className="grid--third">
-            <label for="first-name">First Name</label>
-            <input type="text" id="first-name"/>
-          </fieldset>
-          <fieldset className="grid--third">
-            <label for="middle-name">Middle Name</label>
-            <input type="text" id="middle-name"/>
-          </fieldset>
-          <fieldset className="grid--third">
-            <label for="last-name">Last Name</label>
-            <input type="text" id="last-name"/>
-          </fieldset>
-          <fieldset className="grid--third">
-            <label for="birthday">Date of Birth</label>
-            <input type="text" id="birthday"/>
-          </fieldset>
-          <fieldset className="grid--third select--has-icon">
-            <label for="gender">Gender</label>
-            <select id="gender">
-              <option>Male</option>
-              <option>Female</option>
-            </select>
-          </fieldset>
-          <div>
-            <button>Submit</button>
           </div>
         </div>
       </div>
