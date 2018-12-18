31561e2511c5a25fe48375ef449ec0c7bcec94ef
Austin Knobloch <austin.knobloch@samaritanministries.org>
2018-12-18 09:42:20 -0600

    Overpanel Refactor (#24)
    * Add overpanel styling and example page
    * Add animation to navigation items

---
+++ src/pages/templates/edit-overpanel.js
@@ -0,0 +1,64 @@
+import React from 'react'
+import '../../sass/example-page/example-page.scss'
+
+export default () => (
+
+	<div className="overpanel">
+		<header className="title-bar">
+			<div className="title-content">
+				<div>
+					<h2>Edit Address</h2>
+					<p>Manage the address tied to your membership</p>
+				</div>
+				<button className="button button--secondary"><i className="dashing-icon dashing-icon--close"></i> Close</button>
+			</div>
+		</header>
+
+    <section className="app">
+			<div className="content">
+				<div className="grid grid-padding">
+          <div className="card flex-content">
+
+            <div className="flex-row">
+              <fieldset>
+                <label>Mailing Title</label>
+                <input type="text" for="mailing-title"/>
+              </fieldset>
+            </div>
+            <div className="flex-row">
+              <fieldset>
+                <label>Address</label>
+                <input type="text" for="address"/>
+              </fieldset>
+            </div>
+
+            <div className="flex-row">
+              <fieldset>
+                <label>City</label>
+                <input type="text" for="mailing-title"/>
+              </fieldset>
+              <fieldset>
+                <label>State</label>
+                <input type="text" for="address"/>
+              </fieldset>
+            </div>
+
+            <div className="flex-row">
+              <fieldset>
+                <label>ZIP</label>
+                <input type="text" for="mailing-title"/>
+              </fieldset>
+              <fieldset>
+
+              </fieldset>
+            </div>
+						<div className="flex-row">
+							<button className="button button--primary">Save Address</button>
+						</div>
+
+          </div>
+				</div>
+			</div>
+    </section>
+  </div>
+)
--- src/pages/templates/overpanel.js
+++ src/pages/templates/overpanel.js
@@ -1,18 +1,26 @@
 import React from 'react'
+import { Link } from 'gatsby'
 import '../../sass/example-page/example-page.scss'
 
 export default () => (
 
-	<div>
+	<div className="overpanel">
 		<header className="title-bar">
+			<div className="title-content">
+				<div>
+					<h2>Overpanel Title</h2>
+					<p>Overpanel description</p>
+				</div>
+				<Link to={"/templates/default"}>
+					<button className="button button--secondary"><i className="dashing-icon dashing-icon--close"></i> Close</button>
+				</Link>
+			</div>
 		</header>
 
-    <section className="app">
-			<div className="content content--reader-width">
-				<div className="grid grid-padding">
-		      <h1>Example Fullscreen Page</h1>
-		      <p>This example shows a fullscreen layout. Simple! This example shows a fullscreen layout. Simple! This example shows a fullscreen layout. Simple! This example shows a fullscreen layout. Simple! This example shows a fullscreen layout. Simple! This example shows a fullscreen layout. Simple!</p>
-				</div>
+    <section className="content">
+			<div className="grid grid-padding">
+	      <h2>Example Fullscreen Page</h2>
+	      <p>This example shows a fullscreen layout. Simple! This example shows a fullscreen layout. Simple! This example shows a fullscreen layout. Simple! This example shows a fullscreen layout. Simple! This example shows a fullscreen layout. Simple! This example shows a fullscreen layout. Simple!</p>
 			</div>
     </section>
   </div>
