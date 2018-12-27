46110251f5c320b19ef6ac2adb0e8788a750568e
Austin Knobloch <austin.knobloch@samaritanministries.org>
2018-12-27 10:18:44 -0600

    Adjust animation time and position

--- src/pages/templates/default.js
+++ src/pages/templates/default.js
@@ -7,16 +7,18 @@ export default () => (
 
 <div>
 	<main>
-	<div className="toast-container">
-		<div className="toast-message success">
-			<i className="toast-message--icon"></i>
-			<h4>Success</h4>
-			<p>Address saved successfully</p>
-			<button class="button button--icon toast-message--close">
-				<i className="dashing-icon dashing-icon--close"></i>
-			</button>
+
+		<div className="toast-container">
+			<div className="toast-message success">
+				<i className="toast-message--icon"></i>
+				<h4>Success</h4>
+				<p>Address saved successfully</p>
+				<button class="button button--icon toast-message--close">
+					<i className="dashing-icon dashing-icon--close"></i>
+				</button>
+			</div>
 		</div>
-	</div>
+
 		<header className="header">
 			<Link to={"/templates/mobile"} className="mobile-sidebar--button" id="close-button"><i className="dashing-icon dashing-icon--menu"></i></Link>
 			<div className="logo"><p className="text-color--white">LOGO</p></div>
