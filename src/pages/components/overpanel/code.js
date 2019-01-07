0e54592aa61318b7ec62ca38669676d6fe77509a
Jared Rauh <jared.rauh@samaritanministries.org>
2019-01-07 09:43:49 -0600

    updated overpanel example language

--- src/pages/components/overpanel/code.js
+++ src/pages/components/overpanel/code.js
@@ -26,21 +26,21 @@ export default () => (
             <Link className="button button--primary" to="../../templates/overpanel-example" style={{ marginBottom: "1rem" }}>View Example Overpanel</Link>
 
             <CodeToggle>
-{`<div className="overpanel">
-  <header className="title-bar">
-    <div className="title-content">
+{`<div class="overpanel">
+  <header class="title-bar">
+    <div class="title-content">
       <div>
         <h2>Overpanel Title</h2>
         <p>Overpanel description</p>
       </div>
-      <Link to={"/templates/default"}>
-        <button className="button button--secondary"><i className="dashing-icon dashing-icon--close"></i> Close</button>
-      </Link>
+      <a href="/templates/default">
+        <button class="button button--secondary"><i class="dashing-icon dashing-icon--close"></i> Close</button>
+      </a>
     </div>
   </header>
 
-  <section className="content">
-    <div className="grid grid-padding">
+  <section class="content">
+    <div class="grid grid-padding">
       <h2>Example Fullscreen Page</h2>
       <p>This example shows a fullscreen layout. Simple! This example shows a fullscreen layout. Simple! This example shows a fullscreen layout. Simple! This example shows a fullscreen layout. Simple! This example shows a fullscreen layout. Simple! This example shows a fullscreen layout. Simple!</p>
     </div>
@@ -50,7 +50,7 @@ export default () => (
           </div>
 
           <h2 className="mt-space-xl">Overpanel Widths</h2>
-          <p className="no-margin">To change the max width of the overpanel all you need to do is apply an additional class <strong>AFTER</strong> <code className="example-text">overpanel</code>.</p>
+          <p className="no-margin">To change the max width of the overpanel and page content, all you need to do is apply an additional class <strong>AFTER</strong> <code className="example-text">overpanel</code>.</p>
           <ul className="no-margin">
             <li><strong>Small Width (350px):</strong> <code className="example-text">overpanel-s</code></li>
             <li><strong>Default Width (800px):</strong> <code className="example-text">overpanel-m</code></li>
@@ -60,7 +60,7 @@ export default () => (
             <div className="show-code">
             <CodeToggle>
 {`<!-- By default the content width is set to overpanel-m -->
-<div className="overpanel overpanel-l">
+<div class="overpanel overpanel-l">
   ...`}
             </CodeToggle>
           </div>
