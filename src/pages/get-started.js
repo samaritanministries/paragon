4a98cb602f549dc2f1025c7a58e6857d38ca0c28
Caleb Bill <Caleb.Bill@samaritanministries.org>
2019-01-21 15:53:09 -0600

    - add bash syntax

--- src/pages/get-started.js
+++ src/pages/get-started.js
@@ -3,6 +3,7 @@ import React from 'react'
 import Layout from '../components/layout'
 import AppContent from '../components/appcontent'
 import CodeToggleSCSS from '../components/codetoggle-scss'
+import 'prismjs/components/prism-bash.min.js'
 
 class GetStarted extends React.Component {
 
