1d332f7dd354cdd18deb199ab92e3a03c446207b
Caleb Bill <Caleb.Bill@samaritanministries.org>
2019-01-24 11:41:15 -0600

    add Link to accordion and actions

--- src/pages/components/accordion/code.js
+++ src/pages/components/accordion/code.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import $ from 'jquery';
+import { Link } from 'gatsby'
 
 import Layout from '../../../components/layout'
 import SubNav from '../../../components/subnavigation'
