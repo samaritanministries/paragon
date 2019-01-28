6ecd3872145ed4f275ad289275abd34183b62e92
nevren <nevren89@hotmail.com>
2019-01-28 16:18:12 -0600

    change subnavigation for new url links, add svg favicon, fix reference case causing problems on Windows OS

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Link } from 'gatsby'
 
-import NotFoundLayout from '../components/NotFoundLayout'
+import NotFoundLayout from '../components/notfoundlayout'
 import AppContent from '../components/appcontent'
 
 export default () => (
