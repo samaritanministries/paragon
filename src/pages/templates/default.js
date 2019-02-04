3dc548c0e59f721bc984ab5b29abd4a7e29506cc
Caleb Bill <Caleb.Bill@samaritanministries.org>
2019-02-04 10:24:39 -0600

    - import placeholder and mobile menu per module needing them - remove mobile nav from example pages

--- src/pages/templates/default.js
+++ src/pages/templates/default.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import { Link } from 'gatsby'
 // The scss needs to be imported here for running 'gatsby build'
+import '../../sass/experimental/placeholder.scss'
+import '../../sass/experimental/mobilemenu.scss'
 import '../../sass/example-page/example-page.scss'
 
 export default () => (
