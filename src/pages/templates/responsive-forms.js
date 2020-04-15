0df13bdf55028a0db0a25d138d1bf85a8b2beab2
Austin Knobloch <austin.knobloch@samaritanministries.org>
2020-04-15 10:26:35 -0500

    Initial update

--- src/pages/templates/responsive-forms.js
+++ src/pages/templates/responsive-forms.js
@@ -94,7 +94,7 @@ export default () => (
 					</div>
 				</div>
 
-				<div className="show-code">
+				{/* <div className="show-code">
 					<CodeToggle>
 {`<div className="card-content flex-content">
 	<div className="flex-row">
@@ -137,16 +137,62 @@ export default () => (
 	</div>
 </div>`}
 						</CodeToggle>
-					</div>
+					</div> */}
 
 			</div>
 
 			<div className="grid--half">
-				<h3>Changes</h3>
-				<ul>
-					<li>Give <code className="example-text">fieldset</code> in flex-row's margin left instead of right so when they wrap it lines up correctly.</li>
-					<li>Give the flex-row negative left margin to offset the fieldet left margins</li>
-				</ul>
+				<div className="card">
+					<div className="card-header has-border">
+						<h2 className="no-margin">Flex form updates</h2>
+					</div>
+
+					<div className="card-content flex-content">
+						<p className="mb-space-m">Gives fieldsets a min-width. They will break to the next line if they get to small.</p>
+						<div className="flex-row">
+							<fieldset className="flex-column-1">
+								<label htmlFor="first-name">First Name</label>
+								<input type="text" id="first-name"/>
+							</fieldset>
+							<fieldset className="flex-column">
+								<label htmlFor="middle-name">Middle Name</label>
+								<input type="text" id="middle-name"/>
+							</fieldset>
+							<fieldset>
+								<label htmlFor="last-name">Last Name</label>
+								<input type="text" id="last-name"/>
+							</fieldset>
+						</div>
+						<div className="flex-row">
+							<fieldset>
+								<label htmlFor="birthday">Date of Birth</label>
+								<input type="date" id="birthday"/>
+							</fieldset>
+							<fieldset className="select--has-icon">
+								<label htmlFor="gender">Gender</label>
+								<select id="gender">
+									<option>Male</option>
+									<option>Female</option>
+								</select>
+							</fieldset>
+							<fieldset className="spacer"></fieldset>
+						</div>
+						<div className="flex-row">
+							<fieldset className="select--has-icon">
+								<label htmlFor="Occupation">Occupation</label>
+								<select id="Occupation">
+									<option>1</option>
+									<option>2</option>
+								</select>
+							</fieldset>
+							<fieldset className="spacer"></fieldset>
+						</div>
+					</div>
+					<div className="card-footer">
+						<button className="mr-space-xs">Submit</button>
+						<button className="button--transparent button--gray">Cancel</button>
+					</div>
+				</div>
 			</div>
 
 			{/* <div className="grid--half">
