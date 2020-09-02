de972dfec978f252327b1d0826913ea6afc50116
Austin Knobloch <austin.knobloch@samaritanministries.org>
2020-09-02 12:46:10 -0500

    Add functionality to use a Title in the Subnavigation (#137)

--- src/pages/components/navigation/code.js
+++ src/pages/components/navigation/code.js
@@ -6,6 +6,7 @@ import Layout from '../../../components/layout'
 import SubNav from '../../../components/subnavigation'
 import AppContent from '../../../components/appcontent'
 import CodeToggle from '../../../components/codetoggle'
+import SubNavigation from './subnav'
 
 const currentPageName = "Navigation";
 const currentPageNameLower = currentPageName.toLowerCase();
@@ -96,6 +97,48 @@ class NavigationCode extends React.Component {
 </nav>`}
               </CodeToggle>
               </div>
+
+              <h2 className="mt-space-xl" id="subnavigation">Subnavigation with Title
+                <Link to={location.pathname + "/#subnavigation-with-title"} className="button button--transparent button--copy-link"></Link>
+              </h2>
+              <div className="example-container relative">
+
+                <header className="header">
+              		<nav className="header-nav">
+              			<ul>
+              				<li><Link to={"/components/navigation/code"} className="active">Page 1</Link></li>
+              				<li><Link to={"/components/navigation/code"}>Page 2</Link></li>
+              				<li><Link to={"/components/navigation/code"}>Page 3</Link></li>
+              			</ul>
+              		</nav>
+              	</header>
+                <SubNavigation title="Settings" className="mb-space-m">
+        					<li><Link to={"/components/navigation/code"} className="active">Option 1</Link></li>
+        					<li><Link to={"/components/navigation/code"}>Option 2</Link></li>
+        					<li><Link to={"/components/navigation/code"}>Option 3</Link></li>
+        				</SubNavigation>
+
+                <CodeToggle>
+  {`<header className="header">
+    <nav class="header-nav">
+      <ul>
+        <li><Link to={"/"} class="active">Page 1</Link></li>
+        <li><Link to={"/"}>Page 2</Link></li>
+        <li><Link to={"/"}>Page 3</Link></li>
+      </ul>
+    </nav>
+  </header>
+  <nav class="sub-nav has-title">
+    <div class="sub-nav--title">Settings</div>
+    <ul class="subnav-dropdown">
+      <li><Link to={"/"} class="active">Option 1</Link></li>
+      <li><Link to={"/"}>Option 2</Link></li>
+      <li><Link to={"/"}>Option 3</Link></li>
+      <li><Link to={"/"}>Option 4</Link></li>
+    </ul>
+  </nav>`}
+                </CodeToggle>
+                </div>
 
             </div>
         </AppContent>
---
+++ src/pages/components/navigation/subnav.js
@@ -0,0 +1,74 @@
+import cn from "classnames";
+import React from 'react'
+import PropTypes from 'prop-types'
+
+export default class SubNavigation extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = { hideMenu: true };
+    this.toggleMenu = this.toggleMenu.bind(this);
+  }
+
+  render() {
+    const SubNavigation = this.SubNavigation.bind(this);
+
+    return (
+      <SubNavigation />
+    );
+  }
+
+  SubNavigation() {
+    const SubNavTitle = this.SubNavTitle.bind(this);
+    const { children, className, listClassName, title } = this.props;
+
+    return (
+      <nav
+        className={cn("sub-nav", {
+          [className]: !!className,
+          "has-title": title != null
+        })}
+      >
+        {title != null && <SubNavTitle/>}
+        <ul
+          className={cn({
+          [listClassName]: !!listClassName,
+          "subnav-dropdown": title != null,
+          "hide-menu": this.state.hideMenu
+        })}>
+          {children}
+        </ul>
+      </nav>
+    );
+  }
+
+  SubNavTitle() {
+    const { title } = this.props;
+    const toggleMenu = this.toggleMenu.bind(this);
+
+    return (
+      <div className="sub-nav--title" onClick={this.toggleMenu}>
+        {title}
+        <i className="dashing-icon dashing-icon--arrow-down dashing-icon--purple sub-nav--icon" />
+      </div>
+    );
+  }
+
+  toggleMenu() {
+    this.setState(prevState => ({
+      hideMenu: !prevState.hideMenu
+    }));
+   }
+}
+
+SubNavigation.propTypes = {
+  className: PropTypes.string,
+  listClassName: PropTypes.string,
+  title: PropTypes.string
+};
+
+SubNavigation.defaultProps = {
+  className: undefined,
+  listClassName: undefined,
+  message: undefined
+};
