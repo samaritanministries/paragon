984d846598ed342dfe2a5836bd1eb9a4c9c4f1de
Nevren <nevren89@hotmail.com>
2021-02-08 13:13:25 -0600

    refactor - update configs

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,40 +1,40 @@
 module.exports = {
   siteMetadata: {
-    title: 'Paragon',
-    description: `Style Framework for Samaritan Ministries`
+    title: "Paragon",
+    description: "Style Framework for Samaritan Ministries"
   },
   plugins: [
-    `gatsby-plugin-sass`,
-    'gatsby-plugin-react-helmet',
+    "gatsby-plugin-sass",
+    "gatsby-plugin-react-helmet",
     {
-     resolve: `gatsby-plugin-google-fonts`,
-     options: {
-       fonts: [
-         `open sans:600,400,300` // you can also specify font weights and styles
-       ]
-     }
-   },
-   {
-     resolve: `gatsby-remark-prismjs`,
-     options: {
-       classPrefix: "language-",
-       inlineCodeMarker: null,
-       showLineNumbers: false,
-       noInlineHighlight: false,
-     },
-   },
-   {
-      resolve: `gatsby-plugin-manifest`,
+      resolve: "gatsby-plugin-google-fonts",
       options: {
-        name: `Paragon Framework`,
-        short_name: `Paragon`,
-        start_url: `/`,
-        background_color: `#EDEDED`,
-        theme_color: `#28336D`,
-        display: `standalone`,
-        icon: `./src/img/favicon.svg`, // This path is relative to the root of the site.
-        include_favicon: true, // Include favicon
-      },
+        fonts: [
+          "open sans:600,400,300" // you can also specify font weights and styles
+        ]
+      }
     },
-  ],
-}
+    {
+      resolve: "gatsby-remark-prismjs",
+      options: {
+        classPrefix: "language-",
+        inlineCodeMarker: null,
+        showLineNumbers: false,
+        noInlineHighlight: false
+      }
+    },
+    {
+      resolve: "gatsby-plugin-manifest",
+      options: {
+        name: "Paragon Framework",
+        short_name: "Paragon",
+        start_url: "/",
+        background_color: "#EDEDED",
+        theme_color: "#28336D",
+        display: "standalone",
+        icon: "./src/img/favicon.svg", // This path is relative to the root of the site.
+        include_favicon: true // Include favicon
+      }
+    }
+  ]
+};
