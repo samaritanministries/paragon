c1af13fa3db3efa0df6b329ed8d7b92c6865c407
Austin Knobloch <austin.knobloch@samaritanministries.org>
2022-01-28 08:01:14 -0600

    Add toggle that saves state

--- src/components/toggle-dark-mode.js
+++ src/components/toggle-dark-mode.js
@@ -7,19 +7,64 @@ class ToggleDarkMode extends React.Component {
 
   constructor(props) {
     super(props);
+    this.state = { togClass: 'dark' };
     this.toggleDarkMode = this.toggleDarkMode.bind(this);
   }
 
+  setTheme(themeName) {
+    localStorage.setItem('theme', themeName);
+    document.documentElement.className = themeName;
+  }
+
+  setDark() {
+    this.setTheme('theme-dark');
+    document.body.classList.add(darkModeClass);
+    this.setState({togClass: 'dark'})
+  }
+  setLight() {
+    this.setTheme('theme-light')
+    document.body.classList.remove(darkModeClass);
+    this.setState({togClass: 'light'})
+  }
+
+  keepTheme() {
+    if (localStorage.getItem('theme')) {
+      if (localStorage.getItem('theme') === 'theme-dark') {
+        this.setDark();
+      } else if (localStorage.getItem('theme') === 'theme-light') {
+        this.setLight();
+      }
+    } else {
+      this.setDark();
+    }
+  }
+
+  componentDidMount() {
+    this.keepTheme();
+  }
+
   toggleDarkMode() {
-    document.body.classList.toggle(darkModeClass);
+    if (localStorage.getItem('theme') === 'theme-dark') {
+      this.setLight();
+    } else {
+      this.setDark();
+    }
   }
 
   render() {
-
+    const themeName = localStorage.getItem('theme');
     return (
-      <button className="button button--orange button--icon theme-switcher" onClick={this.toggleDarkMode}>
-        <i className="dashing-icon dashing-icon--star-filled mr-space-xs" />Mode
-      </button>
+      <>
+        {themeName === "theme-light" ?
+        <button className="button button--red button--icon theme-switcher" onClick={this.toggleDarkMode}>
+          <i className="dashing-icon dashing-icon--checkbox" /> Light
+        </button>
+          :
+        <button className="button button--blue button--icon theme-switcher" onClick={this.toggleDarkMode}>
+          <i className="dashing-icon dashing-icon--checkbox" /> Dark
+        </button>
+        }
+      </>
     );
   }
 
